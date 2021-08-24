f3e3b7948c6ba65cdb1b027795c89ceae85d3ab0
alexalspaw-tekcamp <aalspaw@teksystems.com>
2021-08-24 08:31:35 -0500

    Dog constructor

---
+++ src/com/teksystems/constructors/DogParametizedConstructor.java
@@ -0,0 +1,37 @@
+package com.teksystems.constructors;
+
+public class DogParametizedConstructor {
+    private String breed;
+    private int age;
+    private String color;
+
+    public DogParametizedConstructor(String breed, int age, String color) {
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
